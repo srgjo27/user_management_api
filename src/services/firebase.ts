4c5d470b75148519c2e6a917c7a01b5bf92fc8d1
josuasr <josua.s@avowsit.com>
2025-07-30 13:37:17 +0700

    fix: use .env configuration

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,9 +1,12 @@
 import * as admin from 'firebase-admin';
+import dotenv from 'dotenv';
 
-import serviceAccount from '../../serviceAccountKey.json';
+dotenv.config();
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY ?? ' ');
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as any),
+    credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
